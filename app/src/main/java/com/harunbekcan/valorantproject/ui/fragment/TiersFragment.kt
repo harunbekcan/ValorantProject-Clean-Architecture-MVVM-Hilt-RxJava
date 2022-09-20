d8f2265a29b286d90eb6bea29c1637c0cc39e795
Harun Bekcan <harunbekcan@gmail.com>
2022-09-20 23:09:14 +0300

    bottomNavComponent added

--- app/src/main/java/com/harunbekcan/valorantproject/ui/fragment/TiersFragment.kt
+++ app/src/main/java/com/harunbekcan/valorantproject/ui/fragment/TiersFragment.kt
@@ -3,12 +3,11 @@ package com.harunbekcan.valorantproject.ui.fragment
 import android.os.Bundle
 import com.harunbekcan.valorantproject.R
 import com.harunbekcan.valorantproject.base.BaseFragment
-import com.harunbekcan.valorantproject.databinding.FragmentMapsBinding
+import com.harunbekcan.valorantproject.databinding.FragmentTiersBinding
 
+class TiersFragment : BaseFragment<FragmentTiersBinding>() {
 
-class MapsFragment : BaseFragment<FragmentMapsBinding>() {
-
-    override fun getLayoutId(): Int = R.layout.fragment_maps
+    override fun getLayoutId(): Int = R.layout.fragment_tiers
 
     override fun prepareView(savedInstanceState: Bundle?) {}
 
--- app/src/main/java/com/harunbekcan/valorantproject/ui/fragment/WeaponsFragment.kt
+++ app/src/main/java/com/harunbekcan/valorantproject/ui/fragment/WeaponsFragment.kt
@@ -1,10 +1,6 @@
 package com.harunbekcan.valorantproject.ui.fragment
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import com.harunbekcan.valorantproject.R
 import com.harunbekcan.valorantproject.base.BaseFragment
 import com.harunbekcan.valorantproject.databinding.FragmentWeaponsBinding
