494dc998293d74cc94142d52fd9e278ae3cb0ccb
Harun Bekcan <harunbekcan@gmail.com>
2022-09-24 21:14:37 +0300

    serviceInterface

--- app/src/main/java/com/harunbekcan/valorantproject/data/api/ServiceInterface.kt
+++ app/src/main/java/com/harunbekcan/valorantproject/data/api/ServiceInterface.kt
@@ -1,10 +1,11 @@
 package com.harunbekcan.valorantproject.data.api
 
 import com.harunbekcan.valorantproject.data.response.AgentResponse
+import io.reactivex.rxjava3.core.Single
 import retrofit2.http.GET
 
 interface ServiceInterface {
 
-    @GET("v1/agents/?isPlayableCharacter=true")
-    suspend fun getAgents(): AgentResponse
+    @GET("agents/?isPlayableCharacter=true")
+    suspend fun getAgents(): Single<AgentResponse>
 }
