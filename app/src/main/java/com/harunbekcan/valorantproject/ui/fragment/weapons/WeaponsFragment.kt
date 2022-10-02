78eb61ff945085226f53184cd716336cc4a9a745
Harun Bekcan <harunbekcan@gmail.com>
2022-10-02 15:21:03 +0300

    weaponsPage done

--- app/src/main/java/com/harunbekcan/valorantproject/ui/fragment/weapons/WeaponsFragment.kt
+++ app/src/main/java/com/harunbekcan/valorantproject/ui/fragment/weapons/WeaponsFragment.kt
@@ -1,14 +1,36 @@
 package com.harunbekcan.valorantproject.ui.fragment.weapons
 
 import android.os.Bundle
+import androidx.fragment.app.viewModels
 import com.harunbekcan.valorantproject.R
 import com.harunbekcan.valorantproject.base.BaseFragment
 import com.harunbekcan.valorantproject.databinding.FragmentWeaponsBinding
+import com.harunbekcan.valorantproject.ui.adapter.weapons.WeaponsAdapter
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class WeaponsFragment : BaseFragment<FragmentWeaponsBinding>() {
+
+    private lateinit var weaponsAdapter: WeaponsAdapter
+    private val viewModel : WeaponsViewModel by viewModels()
 
     override fun getLayoutId(): Int = R.layout.fragment_weapons
 
-    override fun prepareView(savedInstanceState: Bundle?) {}
+    override fun prepareView(savedInstanceState: Bundle?) {
+        viewModel.weaponsRequest()
+        initWeaponsResponseObserve()
+    }
+
+    private fun initAdapter(){
+        weaponsAdapter = WeaponsAdapter(viewModel.getWeaponsAdapterList())
+        binding.weaponsRecyclerView.adapter = weaponsAdapter
+    }
+
+    private fun initWeaponsResponseObserve(){
+        viewModel.weaponsResponseObserve.observe(viewLifecycleOwner){
+            initAdapter()
+            viewModel.mapOnWeaponsResponse(it)
+        }
+    }
 
 }
