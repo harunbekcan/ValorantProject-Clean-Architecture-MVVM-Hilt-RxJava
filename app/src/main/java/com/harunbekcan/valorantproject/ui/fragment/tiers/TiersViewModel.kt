d80365e3eab51d499bb91d110b9292cf152ce73b
Harun Bekcan <harunbekcan@gmail.com>
2022-10-03 23:35:25 +0300

    tiersViewModel

--- app/src/main/java/com/harunbekcan/valorantproject/ui/fragment/tiers/TiersViewModel.kt
+++ app/src/main/java/com/harunbekcan/valorantproject/ui/fragment/tiers/TiersViewModel.kt
@@ -1,10 +1,11 @@
 package com.harunbekcan.valorantproject.ui.fragment.tiers
 
 import androidx.lifecycle.ViewModel
+import com.harunbekcan.valorantproject.ui.mapper.tiers.TiersMapper
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
-class TiersViewModel @Inject constructor() : ViewModel() {
+class TiersViewModel @Inject constructor( private val tiersMapper: TiersMapper) : ViewModel() {
 
 }
