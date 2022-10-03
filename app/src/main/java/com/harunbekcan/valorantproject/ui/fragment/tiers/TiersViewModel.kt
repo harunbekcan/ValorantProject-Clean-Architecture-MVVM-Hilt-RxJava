a70249a8de4a3602057789d8400a0bb5b00a9616
Harun Bekcan <harunbekcan@gmail.com>
2022-10-03 23:33:50 +0300

    tiersViewModel created

---
+++ app/src/main/java/com/harunbekcan/valorantproject/ui/fragment/tiers/TiersViewModel.kt
@@ -0,0 +1,10 @@
+package com.harunbekcan.valorantproject.ui.fragment.tiers
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class TiersViewModel @Inject constructor() : ViewModel() {
+
+}
