3ee799708cf716010e803ba59c9e69f1c084318c
Harun Bekcan <harunbekcan@gmail.com>
2022-10-04 23:13:20 +0300

    tiersPage

--- app/src/main/java/com/harunbekcan/valorantproject/ui/fragment/tiers/TiersFragment.kt
+++ app/src/main/java/com/harunbekcan/valorantproject/ui/fragment/tiers/TiersFragment.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import com.harunbekcan.valorantproject.R
 import com.harunbekcan.valorantproject.base.BaseFragment
 import com.harunbekcan.valorantproject.databinding.FragmentTiersBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class TiersFragment : BaseFragment<FragmentTiersBinding>() {
 
     override fun getLayoutId(): Int = R.layout.fragment_tiers
