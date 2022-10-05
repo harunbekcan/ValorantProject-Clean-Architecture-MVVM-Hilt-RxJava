19885111084978272eac1842fddc1e50efd2eef0
Harun Bekcan <harunbekcan@gmail.com>
2022-10-05 21:17:32 +0300

    tiersMapper added

---
+++ app/src/main/java/com/harunbekcan/valorantproject/data/uimodel/tiers/TiersItem.kt
@@ -0,0 +1,6 @@
+package com.harunbekcan.valorantproject.data.uimodel.tiers
+
+data class TiersItem(
+    var tierIcon: String ?= null,
+    var tierName: String ?= null
+)
--- app/src/main/java/com/harunbekcan/valorantproject/ui/mapper/tiers/TiersMapper.kt
+++ app/src/main/java/com/harunbekcan/valorantproject/ui/mapper/tiers/TiersMapper.kt
@@ -1,7 +1,35 @@
 package com.harunbekcan.valorantproject.ui.mapper.tiers
 
+import com.harunbekcan.valorantproject.data.response.tiers.CompetitiveTierResponse
+import com.harunbekcan.valorantproject.data.response.tiers.TiersResponse
+import com.harunbekcan.valorantproject.data.uimodel.tiers.TiersItem
 import javax.inject.Inject
 
 open class TiersMapper @Inject constructor() {
 
+    private var tiersAdapterList = ArrayList<TiersItem>()
+    fun getTiersAdapterList() = tiersAdapterList
+
+    fun mapOnTiersResponse(tiersResponse: TiersResponse){
+        tiersAdapterList.clear()
+        addTiersItem(tiersResponse)
+    }
+
+    private fun tiersResponseConvertToModel(competitiveTierResponse: CompetitiveTierResponse): TiersItem {
+        return TiersItem().apply {
+            competitiveTierResponse.tiers.forEach { item->
+                item.tierName.let { tierName-> this.tierName = tierName }
+                item.largeIcon.let { tierName-> this.tierIcon = tierName }
+            }
+        }
+    }
+
+    private fun addTiersItem(tiersResponse: TiersResponse) {
+        tiersResponse.data.forEach { competitiveTierResponse ->
+            competitiveTierResponse.tiers.forEach { item->
+                val tiersItem = tiersResponseConvertToModel(competitiveTierResponse)
+                tiersAdapterList.add(tiersItem)
+            }
+        }
+    }
 }
