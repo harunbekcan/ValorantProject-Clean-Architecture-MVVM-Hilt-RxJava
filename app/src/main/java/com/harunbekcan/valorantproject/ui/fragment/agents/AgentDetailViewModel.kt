79935fa3b9e4447a932c8401468de20b48c4b1e0
Harun Bekcan <harunbekcan@gmail.com>
2022-10-07 22:49:59 +0300

    agentDetailViewModel created

---
+++ app/src/main/java/com/harunbekcan/valorantproject/ui/fragment/agents/AgentDetailViewModel.kt
@@ -0,0 +1,9 @@
+package com.harunbekcan.valorantproject.ui.fragment.agents
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class AgentDetailViewModel @Inject constructor() : ViewModel() {
+}
