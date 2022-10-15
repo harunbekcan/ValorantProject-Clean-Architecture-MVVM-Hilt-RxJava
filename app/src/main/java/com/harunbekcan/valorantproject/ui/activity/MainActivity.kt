968f2078c194ac95135c816760516a7da198a9dc
Harun Bekcan <harunbekcan@gmail.com>
2022-10-15 19:02:34 +0300

    bottomNavBar fixed

--- app/src/main/java/com/harunbekcan/valorantproject/ui/activity/MainActivity.kt
+++ app/src/main/java/com/harunbekcan/valorantproject/ui/activity/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.harunbekcan.valorantproject.ui.activity
 
 import android.os.Bundle
+import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
 import com.harunbekcan.valorantproject.R
 import com.harunbekcan.valorantproject.base.BaseActivity
 import com.harunbekcan.valorantproject.databinding.ActivityMainBinding
+import com.harunbekcan.valorantproject.utils.setGone
+import com.harunbekcan.valorantproject.utils.setVisible
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -20,5 +23,29 @@ class MainActivity : BaseActivity<ActivityMainBinding>() {
     private fun initNavController(){
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
         NavigationUI.setupWithNavController(binding.bottomNavigationView,navHostFragment.navController)
+        onDestinationChanged(navHostFragment.navController)
+    }
+
+    private fun onDestinationChanged(navController: NavController){
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+
+            when (destination.id) {
+                R.id.agentsFragment -> {
+                    binding.bottomNavigationView.setVisible()
+                }
+                R.id.mapsFragment -> {
+                    binding.bottomNavigationView.setVisible()
+                }
+                R.id.weaponsFragment -> {
+                    binding.bottomNavigationView.setVisible()
+                }
+                R.id.tiersFragment -> {
+                    binding.bottomNavigationView.setVisible()
+                }
+                else -> {
+                    binding.bottomNavigationView.setGone()
+                }
+            }
+        }
     }
 }
--- app/src/main/java/com/harunbekcan/valorantproject/utils/ViewExtensions.kt
+++ app/src/main/java/com/harunbekcan/valorantproject/utils/ViewExtensions.kt
@@ -1,8 +1,18 @@
 package com.harunbekcan.valorantproject.utils
 
+import android.view.View
 import android.widget.ImageView
 import com.bumptech.glide.Glide
 import com.harunbekcan.valorantproject.R
+
+fun View.setGone(){
+    this.visibility  = View.GONE
+}
+
+fun View.setVisible(){
+    this.visibility  = View.VISIBLE
+}
+
 
 fun ImageView.loadImage(imageUrl: String?) {
     Glide.with(this.context)
