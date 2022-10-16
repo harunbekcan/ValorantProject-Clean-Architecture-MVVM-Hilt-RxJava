91a9a126f6c32de91e6ab1fea560a7c3fc8b3919
Harun Bekcan <harunbekcan@gmail.com>
2022-10-16 16:57:50 +0300

    weaponDetailViewModel created

---
+++ app/src/main/java/com/harunbekcan/valorantproject/ui/fragment/weapons/WeaponDetailViewModel.kt
@@ -0,0 +1,10 @@
+package com.harunbekcan.valorantproject.ui.fragment.weapons
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class WeaponDetailViewModel @Inject constructor() : ViewModel() {
+
+}
