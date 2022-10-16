e3c5e7070946a6896c58d4406dca064cfb992ad0
Harun Bekcan <harunbekcan@gmail.com>
2022-10-16 16:55:42 +0300

    weaponDetailPage created

---
+++ app/src/main/java/com/harunbekcan/valorantproject/ui/fragment/weapons/WeaponDetailFragment.kt
@@ -0,0 +1,13 @@
+package com.harunbekcan.valorantproject.ui.fragment.weapons
+
+import android.os.Bundle
+import com.harunbekcan.valorantproject.R
+import com.harunbekcan.valorantproject.base.BaseFragment
+import com.harunbekcan.valorantproject.databinding.FragmentWeaponDetailBinding
+
+class WeaponDetailFragment : BaseFragment<FragmentWeaponDetailBinding>() {
+
+    override fun getLayoutId(): Int = R.layout.fragment_weapon_detail
+
+    override fun prepareView(savedInstanceState: Bundle?) {}
+}
