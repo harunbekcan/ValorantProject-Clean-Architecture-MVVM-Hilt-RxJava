35238f2fab879938b037bd4abcfb33f3edb1869e
Harun Bekcan <harunbekcan@gmail.com>
2022-10-18 21:54:12 +0300

    tiersViewModel fixed

--- app/src/main/java/com/harunbekcan/valorantproject/ui/fragment/tiers/TiersViewModel.kt
+++ app/src/main/java/com/harunbekcan/valorantproject/ui/fragment/tiers/TiersViewModel.kt
@@ -1,8 +1,9 @@
 package com.harunbekcan.valorantproject.ui.fragment.tiers
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.harunbekcan.valorantproject.data.response.tiers.TiersResponse
+import com.harunbekcan.valorantproject.data.uimodel.tiers.TiersItem
 import com.harunbekcan.valorantproject.data.usecase.tiers.TiersUseCase
 import com.harunbekcan.valorantproject.ui.mapper.tiers.TiersMapper
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -11,20 +12,18 @@ import javax.inject.Inject
 @HiltViewModel
 class TiersViewModel @Inject constructor( private val tiersUseCase: TiersUseCase, private val tiersMapper: TiersMapper) : ViewModel() {
 
-    val tiersResponseObserve = MutableLiveData<TiersResponse>()
-    private val isLoad = MutableLiveData<Boolean>()
+    private val _tiersAdapterList = MutableLiveData<ArrayList<TiersItem>>()
+    val tiersAdapterList : LiveData<ArrayList<TiersItem>> = _tiersAdapterList
 
     fun tiersRequest() {
         tiersUseCase.execute(
             onSuccess = {
-                isLoad.value = true
-                tiersResponseObserve.postValue(it)
+                tiersMapper.mapOnTiersResponse(it)
+                _tiersAdapterList.value = tiersMapper.tiersAdapterList
             },
             onError = {
                 it.printStackTrace()
             }
         )
     }
-    fun getTiersAdapterList() = tiersMapper.tiersAdapterList
-    fun mapOnTiersResponse(tiersResponse: TiersResponse) = tiersMapper.mapOnTiersResponse(tiersResponse)
 }
