831489ee360266b6b05d1df0bd343190e2880f0e
Harun Bekcan <harunbekcan@gmail.com>
2022-10-18 22:00:46 +0300

    weaponsViewModel fixed

--- app/src/main/java/com/harunbekcan/valorantproject/ui/fragment/weapons/WeaponsViewModel.kt
+++ app/src/main/java/com/harunbekcan/valorantproject/ui/fragment/weapons/WeaponsViewModel.kt
@@ -1,8 +1,9 @@
 package com.harunbekcan.valorantproject.ui.fragment.weapons
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.harunbekcan.valorantproject.data.response.weapons.WeaponsResponse
+import com.harunbekcan.valorantproject.data.uimodel.weapons.WeaponsItem
 import com.harunbekcan.valorantproject.data.usecase.weapons.WeaponsUseCase
 import com.harunbekcan.valorantproject.ui.mapper.weapons.WeaponsMapper
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -14,20 +15,18 @@ class WeaponsViewModel @Inject constructor(
     private val weaponsMapper: WeaponsMapper
 ) : ViewModel() {
 
-    val weaponsResponseObserve = MutableLiveData<WeaponsResponse>()
-    private val isLoad = MutableLiveData<Boolean>()
+    private val _weaponsAdapterList = MutableLiveData<ArrayList<WeaponsItem>>()
+    val weaponsAdapterList : LiveData<ArrayList<WeaponsItem>> = _weaponsAdapterList
 
     fun weaponsRequest() {
         weaponsUseCase.execute(
             onSuccess = {
-                isLoad.value = true
-                weaponsResponseObserve.postValue(it)
+                weaponsMapper.mapOnWeaponsResponse(it)
+                _weaponsAdapterList.value = weaponsMapper.weaponsAdapterList
             },
             onError = {
                 it.printStackTrace()
             }
         )
     }
-    fun getWeaponsAdapterList() = weaponsMapper.getWeaponsAdapterList()
-    fun mapOnWeaponsResponse(weaponsResponse: WeaponsResponse) = weaponsMapper.mapOnWeaponsResponse(weaponsResponse)
 }
