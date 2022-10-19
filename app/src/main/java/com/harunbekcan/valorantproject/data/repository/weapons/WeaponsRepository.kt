b55a52bc5a2f928f2d1eccbe4422f7326576a65a
Harun Bekcan <harunbekcan@gmail.com>
2022-10-19 17:06:46 +0300

    weaponDetailRepo

--- app/src/main/java/com/harunbekcan/valorantproject/data/repository/weapons/WeaponsRepository.kt
+++ app/src/main/java/com/harunbekcan/valorantproject/data/repository/weapons/WeaponsRepository.kt
@@ -1,9 +1,12 @@
 package com.harunbekcan.valorantproject.data.repository.weapons
 
+import com.harunbekcan.valorantproject.data.response.weapons.WeaponDetailResponse
 import com.harunbekcan.valorantproject.data.response.weapons.WeaponsResponse
 import io.reactivex.rxjava3.core.Single
 
 interface WeaponsRepository {
 
     fun getWeapons(): Single<WeaponsResponse>
+
+    fun getWeaponDetail(weaponUuid: String): Single<WeaponDetailResponse>
 }
