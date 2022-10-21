14cf53198a0efc19d66baf416331bd312d404119
Harun Bekcan <harunbekcan@gmail.com>
2022-10-21 22:07:42 +0300

    mapDetailViewModel created

---
+++ app/src/main/java/com/harunbekcan/valorantproject/ui/fragment/maps/MapDetailViewModel.kt
@@ -0,0 +1,10 @@
+package com.harunbekcan.valorantproject.ui.fragment.maps
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class MapDetailViewModel @Inject constructor(): ViewModel() {
+
+}
