f329ce128934a2a4e9d32ff0db1ee465edbc57f4
Harun Bekcan <harunbekcan@gmail.com>
2022-10-21 22:05:54 +0300

    mapDetailPage created

---
+++ app/src/main/java/com/harunbekcan/valorantproject/ui/fragment/maps/MapDetailFragment.kt
@@ -0,0 +1,13 @@
+package com.harunbekcan.valorantproject.ui.fragment.maps
+
+import android.os.Bundle
+import com.harunbekcan.valorantproject.R
+import com.harunbekcan.valorantproject.base.BaseFragment
+import com.harunbekcan.valorantproject.databinding.FragmentMapDetailBinding
+
+class MapDetailFragment : BaseFragment<FragmentMapDetailBinding>() {
+
+    override fun getLayoutId(): Int = R.layout.fragment_map_detail
+
+    override fun prepareView(savedInstanceState: Bundle?) {}
+}
