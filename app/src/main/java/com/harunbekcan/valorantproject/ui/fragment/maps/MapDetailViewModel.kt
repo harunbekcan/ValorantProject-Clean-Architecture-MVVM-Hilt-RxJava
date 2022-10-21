5e5b32afa76c7c727a68001b2811cdadc1bf080f
Harun Bekcan <harunbekcan@gmail.com>
2022-10-21 22:09:15 +0300

    mapDetailMapper created

--- app/src/main/java/com/harunbekcan/valorantproject/ui/fragment/maps/MapDetailViewModel.kt
+++ app/src/main/java/com/harunbekcan/valorantproject/ui/fragment/maps/MapDetailViewModel.kt
@@ -1,10 +1,11 @@
 package com.harunbekcan.valorantproject.ui.fragment.maps
 
 import androidx.lifecycle.ViewModel
+import com.harunbekcan.valorantproject.ui.mapper.maps.MapDetailMapper
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
-class MapDetailViewModel @Inject constructor(): ViewModel() {
+class MapDetailViewModel @Inject constructor(private val mapDetailMapper: MapDetailMapper): ViewModel() {
 
 }
---
+++ app/src/main/java/com/harunbekcan/valorantproject/ui/mapper/maps/MapDetailMapper.kt
@@ -0,0 +1,7 @@
+package com.harunbekcan.valorantproject.ui.mapper.maps
+
+import javax.inject.Inject
+
+open class MapDetailMapper @Inject constructor() {
+
+}
