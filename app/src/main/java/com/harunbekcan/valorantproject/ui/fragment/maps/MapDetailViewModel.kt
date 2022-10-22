d32c72ff9695d046bf2a3995e8cc5ba7d8f8255f
Harun Bekcan <harunbekcan@gmail.com>
2022-10-22 14:38:45 +0300

    mapDetailViewModel completed

--- app/src/main/java/com/harunbekcan/valorantproject/ui/fragment/maps/MapDetailViewModel.kt
+++ app/src/main/java/com/harunbekcan/valorantproject/ui/fragment/maps/MapDetailViewModel.kt
@@ -1,11 +1,34 @@
 package com.harunbekcan.valorantproject.ui.fragment.maps
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.harunbekcan.valorantproject.data.uimodel.maps.MapDetailItem
+import com.harunbekcan.valorantproject.data.uimodel.weapons.WeaponDetailItem
+import com.harunbekcan.valorantproject.data.usecase.maps.MapDetailUseCase
 import com.harunbekcan.valorantproject.ui.mapper.maps.MapDetailMapper
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
-class MapDetailViewModel @Inject constructor(private val mapDetailMapper: MapDetailMapper): ViewModel() {
+class MapDetailViewModel @Inject constructor(
+    private val mapDetailUseCase: MapDetailUseCase,
+    private val mapDetailMapper: MapDetailMapper
+) : ViewModel() {
 
+    private val _mapDetailItem = MutableLiveData<MapDetailItem>()
+    val mapDetailItem : LiveData<MapDetailItem> = _mapDetailItem
+
+    fun mapDetailRequest(uuid:String) {
+        mapDetailUseCase.mapUuid(uuid)
+        mapDetailUseCase.execute(
+            onSuccess = {
+                mapDetailMapper.mapOnMapDetailResponse(it)
+                _mapDetailItem.value = mapDetailMapper.mapDetailItem
+            },
+            onError = {
+                it.printStackTrace()
+            }
+        )
+    }
 }
